c815fc25e1b0886aa23ff0805ebad2a0e0de2c1f
Martin Marinov <martin.marinov10@yahoo.com>
2025-02-21 21:03:34 +0000

    Limit wizard movement in game area

--- game-engine.js
+++ game-engine.js
@@ -11,23 +11,25 @@ function newFrame() {
     }
 }
 
+
 function modifyWizardPosition() {
     const wizardElement = document.querySelector('.wizard');
+    const gameArea = document.querySelector('.game-area');
 
     // Wizard movement
-    if (state.controls.KeyA) {
+    if (state.controls.KeyA && state.wizard.x > 0) {
         wizardElement.style.left = `${state.wizard.x -= config.speed}px`;
     }
 
-    if (state.controls.KeyD) {
+    if (state.controls.KeyD && state.wizard.x + state.wizard.width < gameArea.offsetWidth) {
         wizardElement.style.left = `${state.wizard.x += config.speed}px`;
     }
 
-    if (state.controls.KeyW) {
+    if (state.controls.KeyW && state.wizard.y > 0) {
         wizardElement.style.top = `${state.wizard.y -= config.speed}px`;
     }
 
-    if (state.controls.KeyS) {
+    if (state.controls.KeyS && state.wizard.y + state.wizard.height < gameArea.offsetHeight) {
         wizardElement.style.top = `${state.wizard.y += config.speed}px`;
     }
 }
